96b0c424bc80aeab7034165bbafbadc1b5674aec
ogtayaliyev <ogtayaliyev9@gmail.com>
2024-03-15 10:25:03 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './Styles/style.css';
+import { BrowserRouter as Router, Routes, Route , Navigate} from 'react-router-dom';
+import HeaderNavbar from './components/navbar/HeaderNavbar';
+import Main from './Pages/main';
+import Footer from './components/Footer/Footer';
+import Register from './Pages/register';
+import Profil from "./Pages/profil";
+import Apropos from "./Pages/apropos";
+import Contact from "./Pages/contact";
+import Cars from "./Pages/cars";
+import Reservationoutils from "./Pages/reservationoutils";
+import Reservationentretien from "./Pages/reservationentretien";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <HeaderNavbar />
+            <Router>
+                <Routes>
+                    <Route path="/home" element={<Main />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/apropos" element={<Apropos />} />
+                    <Route path="/profil" element={<Profil />} />
+                    <Route path="/contact" element={<Contact/>} />
+                    <Route path="/cars" element={<Cars/>} />
+                    <Route path="/reservationentretien" element={<Reservationentretien/>} />
+                    <Route path="/reservationoutils" element={<Reservationoutils/>} />
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                </Routes>
+            </Router>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
