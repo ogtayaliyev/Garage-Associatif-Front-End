f67595a8d1f5f1e621dfe0aeee043135d0ea80c5
ogtayaliyev <ogtayaliyev9@gmail.com>
2024-04-26 14:23:24 +0200

    Projet Fil Rouge

---
+++ src/components/navbar/AuthContext.js
@@ -0,0 +1,32 @@
+// AuthContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const login = () => {
+        // Logique d'authentification, par exemple, enregistrement de l'utilisateur
+        setIsLoggedIn(true);
+    };
+
+    const logout = () => {
+        // Logique de déconnexion
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
